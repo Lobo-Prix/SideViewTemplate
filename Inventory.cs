aa76a23523097ae372ae08a6716085ac570512a0
Lobo Prix <lobo_prix@naver.com>
2018-12-12 21:33:01 +0900

    Item System #1

---
+++ Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Inventory
+{
+    Item[] inventory = new Item[40];
+
+    void AddItem(Item item)
+    {
+        //find item in inventory
+        //if found -> add if item overflow->handle remainder while no remainder
+        //else not found -> handle remainder
+        //if inventory overflow-> throw
+    }
+}
