a68e450354c87993d1e21ecae8c00c12e7696744
Lobo Prix <lobo_prix@naver.com>
2018-12-30 20:40:36 +0900

    Kinematics

--- Assets/Actor.cs
+++ Assets/Actor.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public abstract class Actor : MonoBehaviour
 {
     protected Rigidbody2D rb;
+    Collider2D coll;
     Vector3 ppos;
 
     protected virtual void Start()
     {
         ppos = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     protected virtual void Update()
     {
         Vector3 vec = transform.position - ppos;
         //Debug.DrawRay(ppos, vec, Color.green, 1);
-        if (rb.gravityScale != 0 && rb.velocity.y <= 0 && Physics2D.Raycast(ppos, vec, vec.magnitude * 1.1f, LayerMask.GetMask(new string[] { "JerkWall" })))
+        if (rb.gravityScale != 0 && rb.velocity.y <= 0)
         {
-            transform.position = new Vector3(transform.position.x, ppos.y, transform.position.z);
-            rb.velocity = new Vector2(rb.velocity.x, 0);
+            RaycastHit2D hit = Physics2D.Raycast(ppos, vec, vec.magnitude * 1.1f, LayerMask.GetMask(new string[] { "TriggerWall" }));
+            if (hit /*&& hit.collider.bounds.Intersects()*/)
+            {
+                transform.position = new Vector3(transform.position.x, ppos.y, transform.position.z);
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                hit.transform.SendMessage("OnTriggerEnter2D", coll, SendMessageOptions.DontRequireReceiver);
+            }
         }
         ppos = transform.position;
     }
---
+++ Assets/Kinematics.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematics : MonoBehaviour {
+
+    public Transform start;
+    public Transform dest;
+    public float period = 1.0f;
+    List<Actor> arr = new List<Actor>();
+
+    private void Start()
+    {
+        transform.position = start.position;
+    }
+
+    float tsum = 0;
+    float sign = 1;
+	void Update () {
+        tsum += Time.deltaTime * sign;
+
+        transform.position = (dest.position - start.position) * tsum * 2 / period + start.position;
+
+        if (tsum * 2 >= period)
+            sign *= -1;
+        else if (tsum * 2 < 0)
+            sign = 1;
+
+        var narr = new List<Actor>();
+        foreach(var i in arr)
+        {
+            if (Mathf.Abs(i.transform.position.y - transform.position.y) < 0.2f)
+            {
+                narr.Add(i);
+                i.transform.Translate((dest.position - start.position) * Time.deltaTime * sign * 2 / period);
+            }
+        }
+        arr = narr;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Scaffold scf = coll.GetComponent<Scaffold>();
+        Actor actor = coll.GetComponentInParent<Actor>();
+        if (scf && actor && scf.pos == ScaffoldPos.bottom && !arr.Contains(actor))
+        {
+            foreach (var i in arr)
+                Debug.Log(i.name);
+        }
+
+    }
+}
